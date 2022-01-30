af354f2e144fc757e9de9d828bba0cc0045e6b89
GeorgeYakunenko <Goshan3097@gmail.com>
2022-01-30 18:51:15 +0200

    added promo and partners

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,7 @@ function scripts() {
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'node_modules/rateyo/src/jquery.rateyo.js',
-    'app/js/main.js'
+    'app/js/main.js',
   ])
   .pipe(concat('main.min.js'))
   .pipe(uglify())
